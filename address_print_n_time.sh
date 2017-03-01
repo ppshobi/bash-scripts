5c7a3ad1d4986df9803ac3218059d3c3c465e615
Shobi P P <ppshobi@gmail.com>
2017-03-01 15:30:03 +0530

    Shell Program to print address n times

--- address_print_n_time.sh
+++ address_print_n_time.sh
@@ -3,10 +3,10 @@ echo "Enter Your Address"
 read addr
 
 echo "Enter N"
-read n
+read n 
 i=0
-while [ "$i" -lt "$n" ]
-do 
- echo $addr
- i=$(expr $i+1)
+while (( $i <= $n ))
+do
+	echo "$addr"
+	i=$(( i+1 ))	
 done
